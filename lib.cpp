f6c8576e2be7048ea0b7486412e75487820315fe
Koryun12 <koryunseyranyan>
2025-02-08 01:05:09 +0400

    Lib

---
+++ lib.cpp
@@ -0,0 +1,12 @@
+#include <cstring>
+#ifndef LIBRARY_H
+#define LIBRARY_H
+
+	void AddBook();
+	void ListBook();
+	void SearchBook();
+	void BarrowBook();
+	void ReturnBook();
+	void menu();
+
+#endif 
