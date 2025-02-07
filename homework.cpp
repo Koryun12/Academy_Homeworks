a8c7f78436ca5b20975c14cf7c7c2309e142d917
Koryun12 <koryunseyranyan>
2025-02-08 00:57:00 +0400

    H++

--- homework.cpp
+++ homework.cpp
@@ -1,9 +1,14 @@
-#include<iostream>
-
+#include <iostream>
+#include "library.h"
 
 struct Book {
 	char title[100];
 	char author[100];
 	char isbn[20];
 	int avaliable;
-}
+}book;
+
+	const int max_books = 100;
+	book library[max_books];
+	int bookCount = 0;
+
