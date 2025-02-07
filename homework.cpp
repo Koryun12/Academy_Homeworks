9621111831a37e5fbd67d30655f65ce85012f400
Koryun12 <koryunseyranyan>
2025-02-08 01:19:22 +0400

    H--

--- homework.cpp
+++ homework.cpp
@@ -12,3 +12,21 @@ struct Book {
 	book library[max_books];
 	int bookCount = 0;
 
+	void AddBook(){
+	  if(bookCount >= max_books){
+	std::cout << "The Library Is Full!" << std::endl;
+  }
+	book NewBook;
+	std::cout << "Enter Book Title! ";
+	std::cin.ignore();
+	std::cin.getline(NewBook.title,100);
+	std::cout << "Enter The Author Of Book! ";
+	std::cin.getline(NewBook.author,100);
+	std::cout << "Enter The ISBN Of Book! ";
+	std::cin.getline(NewBook.isbn,20);
+	NewBook.avaliable = 1;
+
+	library[bookCount ++] = NewBook;
+	std::cout << "The Book Succesfully is Done!" << std::endl;
+
+}
