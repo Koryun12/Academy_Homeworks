9375568c9ebd13284901441c0265447424261df0
Koryun12 <koryunseyranyan>
2025-02-27 16:21:41 +0400

    adding

---
+++ Desktop/homework/Leetcode.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int n = s.size();
+        int ans = 0;
+        vector<int> index(128, -1);
+        for (int i = 0, j = 0; j < n; j++) {
+            i = max(index[s[j]] + 1, i);
+            ans = max(ans, j - i + 1);
+            index[s[j]] = j;
+        }
+        return ans;
+        
+    }
+};
